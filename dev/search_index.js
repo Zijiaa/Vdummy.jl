5387b30ae90cb7a38f59725bc781e9f7ba296cd9
Documenter.jl <documenter@juliadocs.github.io>
2022-12-15 18:31:00 +0000

    build based on fb4eba8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Vdummy","category":"page"},{"location":"#Vdummy","page":"Home","title":"Vdummy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Vdummy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Vdummy]","category":"page"}]
+}
